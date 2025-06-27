e41776af5f7506219b862ba73c2234a7c68763c7
rayhan <mohammadrayhanx0@gmail.com>
2025-06-27 20:36:28 +0600

    advance operator done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eztfylz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,8 +31,32 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+       // console.log('pagination',req.query)
+      // ekbar check kore nibho kaj hocce ki na . er por page and size k intejer e convert korbo 
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log('pagination',page,size)
+     
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
         res.send(result);
+    })
+
+    // for orders 
+    app.post('/productByIds',async(req,res)=>{
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id))
+      const query ={
+        _id:{
+          $in: idsWithObjectId
+        }
+      }
+      // console.log(idsWithObjectId)
+      const result = await productCollection.find(query).toArray()
+      res.send(result)
+
     })
 
     app.get('/productsCount',async(req,res)=>{
